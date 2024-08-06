07ff2de82fe3af2e8533d8cc9fba26104af1a36b
tempe <maekawa@tempepe.com>
2024-08-06 22:45:03 +0900

    add intents

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from dowClient import dowClient
+import discord
 
-client = dowClient()
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the intents you need
+
+client = dowClient(intents=intents)
 
 with open('./key', 'r') as f:
     key = f.read()
